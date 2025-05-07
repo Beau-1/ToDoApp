848bad68a5fcab91108efeade86169f9222f5063
James Allen Buchanan <115106802+Beau-1@users.noreply.github.com>
2025-05-07 13:54:23 -0500

    add Firebase configuration and hosting workflows; implement Google login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import ToDoList from "./ToDoList";
 import "./App.css";
+document.addEventListener("DOMContentLoaded", (Event) => {
+    const app = firebase.app();
+});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            const user = result.user;
+            document.write(`Hello ${user.displayName}`);
+            console.log(user);
+        })
+        .catch(console.log);
+}
 
 function App() {
     return <ToDoList />;
