a72c111ef2dceb1933eb14f3765b757d6b7aa839
James Allen Buchanan <115106802+Beau-1@users.noreply.github.com>
2025-05-12 11:27:27 -0500

    refactor: update Login component styles and remove unused CSS file

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,6 @@
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { type Auth } from "firebase/auth";
-import "./Login.css";
+import "./App.css";
 import React, { JSX } from "react";
 
 interface LoginProps {
@@ -20,15 +20,16 @@ function Login({ auth }: LoginProps): JSX.Element {
     return (
         <div className="login-container">
             <div className="login-box">
-                <h1>To Do App</h1>
-                <p>Sign in to continue</p>
-                <button onClick={signInWithGoogle} className="google-btn">
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                        alt="Google logo"
-                    />
-                    Sign in with Google
-                </button>
+                <h1>To Do</h1>
+                <p>
+                    <button onClick={signInWithGoogle} className="google-btn">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                            alt="Google logo"
+                        />
+                        Sign in with Google
+                    </button>
+                </p>
             </div>
         </div>
     );
