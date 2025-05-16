d5aea7da397970c95a5d131addc792dc9411adbb
James Allen Buchanan <115106802+Beau-1@users.noreply.github.com>
2025-05-16 11:33:15 -0500

    fix: update version number to 4.2 in ToDoList component

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -182,7 +182,7 @@ function ToDoList({ db, userId }: ToDoListProps): JSX.Element {
         <main className="to-do-list">
             <header>
                 <h1>To Do</h1>
-                <div className="version">Ver. 4.1</div>
+                <div className="version">Ver. 4.2</div>
             </header>
 
             <form onSubmit={addTask} className="input-container">
